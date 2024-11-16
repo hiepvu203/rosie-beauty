d5f83562b2bd4978f232ad0fd0dd4a96d77441af
Hiep <vuxuanhiep1709@gmail.com>
2024-11-16 15:27:45 +0700

    update

--- app/inc/service.php
+++ app/inc/service.php
@@ -1,25 +1,25 @@
 <link rel="stylesheet" href="app/assets/css/service.css">
 
-<div class="service-container">
+<div class="service-container max-width flex p-20">
     <div class="product-showcase">
         <img src="app/assets/img/service/banner_service.jpg" width="760" height="381" alt="Son siêu xịn" class="product-image">
     </div>
-    <div class="service-features">
-        <div class="feature">
+    <div class="service-features p-20 border-radius-10">
+        <div class="feature flex">
             <img src="app/assets/img/service/thumb_payment.png" alt="Thanh toán" class="feature-icon">
             <div class="feature-text">
                 <h3>Thanh toán</h3>
                 <p>Khách hàng có thể lựa chọn một hoặc nhiều hình thức thanh toán</p>
             </div>
         </div>
-        <div class="feature">
+        <div class="feature flex">
             <img src="app/assets/img/service/thumb_ensure.png" alt="Cam kết chính hãng" class="feature-icon">
             <div class="feature-text">
                 <h3>Cam kết chính hãng</h3>
                 <p>Chúng tôi cam kết hàng chính hãng và đảm bảo về chất lượng sản phẩm</p>
             </div>
         </div>
-        <div class="feature">
+        <div class="feature flex">
             <img src="app/assets/img/service/thumb_transport.png" alt="Siêu tốc 2H" class="feature-icon">
             <div class="feature-text">
                 <h3>Siêu tốc 2H</h3>
