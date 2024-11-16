02f5ae3d063b7799b119e97abf067ea19befba07
Hiep <vuxuanhiep1709@gmail.com>
2024-11-16 15:35:12 +0700

    update

--- app/inc/temp_2.php
+++ app/inc/temp_2.php
@@ -4,11 +4,6 @@
     $result = $stmt->get_result();
 ?>
 
-
-<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
-<!-- <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css"/> -->
-<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/ionicons/7.4.0/esm/ionicons.min.js"></script> -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <style>
     body,input,button,textarea,select {
         outline: none;
@@ -20,10 +15,6 @@
     button {
         cursor: pointer;
     }
-/* 
-    body {
-        padding: 20px;
-    } */
 
     .image-slider {
         padding-bottom: 50px;
